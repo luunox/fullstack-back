9871d7046f1437e9c2be2189c9c46a39c2b67995
luunox <kevinoxxd@gmail.com>
2022-04-12 16:50:58 -0500

    Update knox.routes.js

--- src/routes/knox.routes.js
+++ src/routes/knox.routes.js
@@ -45,7 +45,7 @@ module.exports = function (app) {
 			arrFilter.forEach((dat) => (suma += hoy.diff(dat.nacimiento, 'years')));
 
 			res.json({
-				data: +(Math.round(suma / arrFilter.length + 'e+1') + 'e-1'),
+				data: +(Math.round(suma / arrFilter.length + 'e+2') + 'e-2'),
 				title: 'Cliente creado',
 				message: 'Se agregaron los datos',
 			});
